d0b0e171fde5583ba5e43c44daff7943773c27bf
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-18 11:58:42 +0300

    feat(configuration): Add configuration factory

---
+++ sync/Syncer/Utils/ConfigurationFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace Syncer.Utils
+{
+    public static class ConfigurationFactory
+    {
+        private static IConfigurationBuilder Configure(IConfigurationBuilder config, string environmentName)
+        {
+            return config
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables();
+        }
+
+        /// <summary>
+        /// Use for .NET Core Console applications.
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration CreateConfiguration()
+        {
+            var env = new HostingEnvironment
+            {
+                EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+                ApplicationName = AppDomain.CurrentDomain.FriendlyName,
+                ContentRootPath = AppDomain.CurrentDomain.BaseDirectory,
+                ContentRootFileProvider = new PhysicalFileProvider(AppDomain.CurrentDomain.BaseDirectory)
+            };
+
+            var config = new ConfigurationBuilder();
+            var configured = Configure(config, env.EnvironmentName);
+            return configured.Build();
+        }
+    }
+}
