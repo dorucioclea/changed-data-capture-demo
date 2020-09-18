3a2141fa7cf2d50752544cb3c772a6ed7edd78d5
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-18 16:00:50 +0300

    feat(sync): Add canal configuration option

---
+++ sync/Syncer/Configuration/CanalConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace Syncer.Configuration
+{
+    /// <summary>
+    ///  "CanalConfiguration" : {
+    /// "ServerAddress": "",
+    /// "ServerPort": "",
+    /// "DatabaseName": "",
+    ///    "Credentials": {
+    ///    "UserName": "",
+    ///    "Password": ""
+    ///    }  
+    /// } 
+    /// </summary>
+    public class CanalConfiguration
+    {
+        public string ServerAddress { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public CanalCredentials Credentials { get; set; }
+    }
+
+    public class CanalCredentials
+    {
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
