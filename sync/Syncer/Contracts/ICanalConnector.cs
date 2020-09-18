ccdff3ebc004361c751aecb957485ac8d892ef02
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-18 16:01:55 +0300

    feat(canal): Adds canal connect service

---
+++ sync/Syncer/Contracts/ICanalConnector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Contracts
+{
+    public interface ICanalConnector
+    {
+        void Connect();
+
+
+    }
+}
---
+++ sync/Syncer/Services/CanalConnectorService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CanalSharp.Client.Impl;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Syncer.Configuration;
+using Syncer.Contracts;
+
+namespace Syncer.Services
+{
+    public class CanalConnectorService : ICanalConnector
+    {
+        private readonly ILogger<CanalConnectorService> _logger;
+        private readonly IOptions<CanalConfiguration> _canalConfiguration;
+        private readonly CanalSharp.Client.ICanalConnector _canalConnector;
+
+        public CanalConnectorService(ILogger<CanalConnectorService> logger, IOptions<CanalConfiguration> canalConfiguration)
+        {
+            _logger = logger;
+            _canalConfiguration = canalConfiguration;
+            _canalConnector = ProvideCanalClient(_canalConfiguration.Value);
+        }
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private static CanalSharp.Client.ICanalConnector ProvideCanalClient(CanalConfiguration configuration)
+        {
+            return CanalConnectors.NewSingleConnector(configuration.ServerAddress, configuration.ServerPort,
+                configuration.DatabaseName, configuration.Credentials.UserName,
+                configuration.Credentials.Password);
+        }
+    }
+}
