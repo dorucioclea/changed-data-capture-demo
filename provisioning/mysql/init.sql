c6f71de0fd38d9b5908ec056e53649f4f43a5be5
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 17:26:46 +0300

    feat(mysql): Ensure db initialization

--- provisioning/mysql/init.sql
+++ provisioning/mysql/init.sql
@@ -1,10 +1,13 @@
 
 RESET MASTER;
-CREATE USER 'cdcuser'@'%' IDENTIFIED BY 'cdcpwd';
+SET old_passwords=0;
+CREATE USER 'cdcuser'@'%' IDENTIFIED VIA mysql_native_password USING PASSWORD("cdcpwd");
 -- GRANT REPLICATION SLAVE ON *.* TO 'cdcuser'@'%';
 -- GRANT REPLICATION CLIENT ON *.* TO 'cdcuser'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'cdcuser'@'%' ;
 FLUSH PRIVILEGES;
+
+SET GLOBAL gtid_domain_id = 1;
 
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE IF NOT EXISTS test;
