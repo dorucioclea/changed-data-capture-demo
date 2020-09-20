8a0684fde2007c4a8856a49a2dd1ad7ae72d5aee
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 17:27:23 +0300

    feat(settings): Update settings for local

--- sync/Syncer/Configuration/DatabaseConfiguration.cs
+++ sync/Syncer/Configuration/DatabaseConfiguration.cs
@@ -1,21 +1,14 @@
 ﻿namespace Syncer.Configuration
 {
-    /// <summary>
-    ///  "CanalConfiguration" : {
-    /// "ServerAddress": "",
-    /// "ServerPort": "",
-    /// "DatabaseName": "",
-    ///    "Credentials": {
-    ///    "UserName": "",
-    ///    "Password": ""
-    ///    }  
-    /// } 
-    /// </summary>
     public class DatabaseConfiguration
     {
         public string ServerAddress { get; set; }
 
+        public int ServerId { get; set; }
+
         public int ServerPort { get; set; }
+
+        public string Database { get; set; }
 
         public Credentials Credentials { get; set; }
     }
