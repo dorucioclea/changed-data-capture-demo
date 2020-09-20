6fb026c7c911968634deb3220eee284452f91339
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 18:06:44 +0300

    feat(ordinal-configuration): Add simple classes to hold ordinal configuration

---
+++ sync/Syncer/Configuration/DatabaseOrdinalConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Configuration
+{
+    public class DatabaseOrdinalConfiguration
+    {
+        public List<TableConfiguration> Tables { get; set; }
+    }
+
+    public class TableConfiguration
+    {
+        public string DatabaseName { get; set; }
+
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<ColumnConfiguration> Columns { get; set; }
+    }
+
+    public class ColumnConfiguration
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
