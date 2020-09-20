b2eed3c7fed308f3166c6abb289a97c2b1ec4ccd
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 18:32:05 +0300

    feat(binlog): Binlog create

---
+++ sync/Syncer/Services/Visitors/BinLogCreateVisitor.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MySqlCdc.Events;
+using Syncer.Configuration;
+using Syncer.Contracts;
+
+namespace Syncer.Services.Visitors
+{
+    public class BinLogCreateVisitor : IBinLogEventVisitor
+    {
+        private readonly ILogger<BinLogCreateVisitor> _logger;
+
+        public BinLogCreateVisitor(ILogger<BinLogCreateVisitor> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool CanHandle(IBinlogEvent binLogEvent)
+        {
+            return binLogEvent is WriteRowsEvent;
+        }
+
+        public Task Handle(IBinlogEvent binlogEvent, ExecutionContext executionContext)
+        {
+            var writeRows = binlogEvent as WriteRowsEvent;
+
+            Debug.Assert(writeRows != null, nameof(writeRows) + " != null");
+
+            HandleWriteRowsEvent(writeRows);
+
+            return Task.CompletedTask;
+        }
+
+        private void HandleWriteRowsEvent(WriteRowsEvent writeRows)
+        {
+            var eventString = this.GetBinLogEventJson(writeRows);
+
+            foreach (var _ in writeRows.Rows)
+            {
+                // Do something
+            }
+
+            using (_logger.BeginScope("CreateRowEvent"))
+            {
+                _logger.LogInformation(eventString);
+                _logger.LogInformation($"{writeRows.Rows.Count} rows were written");
+            }
+        }
+    }
+}
