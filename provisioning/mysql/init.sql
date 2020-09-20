26fc32201c0064b8bfce84a82d09a40e2d1063b2
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 15:30:17 +0300

    feat(sql): Add initialization script that creates the cdc user and a dummy table

---
+++ provisioning/mysql/init.sql
@@ -0,0 +1,17 @@
+
+RESET MASTER;
+CREATE USER 'cdcuser'@'%' IDENTIFIED BY 'cdcpwd';
+GRANT REPLICATION SLAVE ON *.* TO 'cdcuser'@'%';
+GRANT REPLICATION CLIENT ON *.* TO 'cdcuser'@'%';
+FLUSH PRIVILEGES;
+
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE IF NOT EXISTS test;
+USE test;
+
+CREATE TABLE `test` (
+        `id` INT(11) NOT NULL,
+        `name` VARCHAR(50) NOT NULL
+)
+ENGINE=InnoDB
+;
