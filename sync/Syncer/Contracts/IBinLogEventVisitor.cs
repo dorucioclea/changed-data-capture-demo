e69472b4529960f087152e2d1c33f02688454b23
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 18:31:53 +0300

    feat(binlog): Add extension method for printing JSON of a binlog event

--- sync/Syncer/Contracts/IBinLogEventVisitor.cs
+++ sync/Syncer/Contracts/IBinLogEventVisitor.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using MySqlCdc.Events;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Syncer.Configuration;
 
 namespace Syncer.Contracts
@@ -9,5 +12,20 @@ namespace Syncer.Contracts
         public bool CanHandle(IBinlogEvent binLogEvent);
 
         public Task Handle(IBinlogEvent binlogEvent, ExecutionContext executionContext);
+    }
+
+    public static class BinLogEventVisitorExtensions
+    {
+        public static string GetBinLogEventJson(this IBinLogEventVisitor _, IBinlogEvent binlogEvent)
+        {
+            var json = JsonConvert.SerializeObject(binlogEvent, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    Converters = new List<JsonConverter> { new StringEnumConverter() }
+                });
+
+            return json;
+        }
     }
 }
