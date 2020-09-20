264c339b16e513cdf1e64e43a4c0ed40112ad16e
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 18:12:01 +0300

    feat(sync): Add a simple execution context

---
+++ sync/Syncer/Configuration/ExecutionContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Syncer.Configuration
+{
+    public class ExecutionContext
+    {
+        public DatabaseOrdinalConfiguration OrdinalConfiguration { get; set; }
+
+        public ExecutionContext()
+        {
+            OrdinalConfiguration = new DatabaseOrdinalConfiguration
+            {
+                Tables = new List<TableConfiguration>()
+            };
+        }
+    }
+}
