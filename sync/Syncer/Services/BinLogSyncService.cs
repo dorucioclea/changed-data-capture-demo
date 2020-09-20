803b79f08fbb7f976a0e4b23dc4932b69e2318fd
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 15:19:05 +0300

    feat(binlog): Remove reference to canal service

--- sync/Syncer/Services/BinLogSyncService.cs
+++ sync/Syncer/Services/BinLogSyncService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Com.Alibaba.Otter.Canal.Protocol;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Syncer.Contracts;
 using Syncer.Entities;
@@ -13,19 +8,16 @@ namespace Syncer.Services
     public class BinLogSyncService : IBinLogSyncService
     {
         private readonly ILogger<BinLogSyncService> _logger;
-        private readonly ICanalConnector _canalConnector;
 
-        public BinLogSyncService(ILogger<BinLogSyncService> logger, ICanalConnector canalConnector)
+        public BinLogSyncService(ILogger<BinLogSyncService> logger)
         {
             _logger = logger;
-            _canalConnector = canalConnector;
         }
 
         public void Initialize()
         {
             _logger.LogInformation("Initializing Canal Client");
 
-            _canalConnector.Connect();
 
             _logger.LogInformation("Canal Client has been initialized successfully");
         }
@@ -33,81 +25,8 @@ namespace Syncer.Services
         public async ValueTask<SyncStatus> Sync()
         {
             await Task.Delay(10);
-            var newMessages = _canalConnector.Get();
 
-            var batchId = newMessages.Id;
-
-            if (batchId == -1 || !newMessages.Entries.Any())
-            {
-                return SyncStatus.NoMessages();
-            }
-
-            PrintEntry(newMessages.Entries);
-
-            return new SyncStatus
-            {
-                Success = true,
-                HasMessages = true
-            };
-        }
-
-        private void PrintEntry(IEnumerable<Entry> entries)
-        {
-            foreach (var entry in entries)
-            {
-                if (entry.EntryType == EntryType.Transactionbegin || entry.EntryType == EntryType.Transactionend)
-                {
-                    continue;
-                }
-
-                RowChange rowChange = null;
-
-                try
-                {
-                    rowChange = RowChange.Parser.ParseFrom(entry.StoreValue);
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e.ToString());
-                }
-
-                if (rowChange == null) continue;
-
-
-                var eventType = rowChange.EventType;
-                _logger.LogInformation(
-                    $"================> binlog[{entry.Header.LogfileName}:{entry.Header.LogfileOffset}] , name[{entry.Header.SchemaName},{entry.Header.TableName}] , eventType :{eventType}");
-
-                foreach (var rowData in rowChange.RowDatas)
-                {
-                    switch (eventType)
-                    {
-                        case EventType.Delete:
-                            PrintColumn(rowData.BeforeColumns.ToList());
-                            break;
-                        case EventType.Insert:
-                            PrintColumn(rowData.AfterColumns.ToList());
-                            break;
-                        default:
-                            _logger.LogInformation("-------> before");
-                            PrintColumn(rowData.BeforeColumns.ToList());
-                            _logger.LogInformation("-------> after");
-                            PrintColumn(rowData.AfterColumns.ToList());
-                            break;
-                    }
-                }
-            }
-        }
-
-        private void PrintColumn(List<Column> columns)
-        {
-            StringBuilder builder = new StringBuilder();
-            foreach (var column in columns)
-            {
-                builder.AppendLine($"{column.Name} ： {column.Value}  update=  {column.Updated}");
-            }
-
-            _logger.LogInformation(builder.ToString());
+            return new SyncStatus();
         }
     }
 }
