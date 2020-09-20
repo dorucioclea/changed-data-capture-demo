1fef352724a9111dc6b6fdda5604f124cd34312c
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-20 18:12:17 +0300

    feat(sync): Add simple bin log event visitor contract

---
+++ sync/Syncer/Contracts/IBinLogEventVisitor.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Configuration;
+
+namespace Syncer.Contracts
+{
+    public interface IBinLogEventVisitor
+    {
+        public bool CanHandle(IBinlogEvent binLogEvent);
+
+        public Task Handle(IBinlogEvent binlogEvent, ExecutionContext executionContext);
+    }
+}
