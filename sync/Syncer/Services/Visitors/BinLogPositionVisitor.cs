218efaa0c09c4ca0a8987e2b02dca1925a19d293
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-09-21 09:56:38 +0300

    refactor(binlog-position): Tidy up code

--- sync/Syncer/Services/Visitors/BinLogPositionVisitor.cs
+++ sync/Syncer/Services/Visitors/BinLogPositionVisitor.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MySqlCdc.Events;
+using Newtonsoft.Json.Bson;
 using Syncer.Configuration;
 using Syncer.Contracts;
 using Syncer.Entities;
@@ -11,31 +13,39 @@ namespace Syncer.Services.Visitors
     public class BinLogPositionVisitor : IBinLogEventVisitor
     {
         private readonly ILogger<BinLogPositionVisitor> _logger;
+        private readonly IOptions<BinLogConfiguration> _binLogConfiguration;
 
-        public BinLogPositionVisitor(ILogger<BinLogPositionVisitor> logger)
+        public BinLogPositionVisitor(ILogger<BinLogPositionVisitor> logger, IOptions<BinLogConfiguration> binLogConfiguration)
         {
             _logger = logger;
+            _binLogConfiguration = binLogConfiguration;
         }
 
         public bool CanHandle(IBinlogEvent binLogEvent)
         {
-            return true;
+            return _binLogConfiguration.Value.AutoSave;
         }
 
         public async Task Handle(EventInfo binlogEvent, ExecutionContext executionContext)
         {
             if (binlogEvent.Options.Filename != null)
             {
-                var binLog = new BinLog(binlogEvent.Options.Filename,  binlogEvent.Options.Position);
-
-                await File.WriteAllTextAsync(executionContext.BinLogLedgerPath,
-                    $"{binLog}");
-
-                _logger.LogDebug($"Logged binlog position { binLog }");
-
-                executionContext.BinLogFilename = binlogEvent.Options.Filename;
-                executionContext.BinLogPosition = binlogEvent.Options.Position;
+                await HandlePosition(binlogEvent, executionContext);
             }
+        }
+
+
+        private async ValueTask HandlePosition(EventInfo binlogEvent, ExecutionContext executionContext)
+        {
+            var binLog = new BinLog(binlogEvent.Options.Filename, binlogEvent.Options.Position);
+
+            await File.WriteAllTextAsync(executionContext.BinLogLedgerPath,
+                $"{binLog}");
+
+            _logger.LogDebug($"Logged binlog position { binLog }");
+
+            executionContext.BinLogFilename = binlogEvent.Options.Filename;
+            executionContext.BinLogPosition = binlogEvent.Options.Position;
         }
     }
 }
