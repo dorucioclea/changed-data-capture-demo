a75b6f90be13ee1a965e6fb909cf4685ac27a7b0
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-04 19:34:35 +0300

    feat(helpers): Add helper attribute to hold map info

---
+++ sync/Syncer/Database/MapToTableAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Syncer.Database
+{
+    public class MapToTableAttribute : Attribute
+    {
+        public string Name { get; }
+
+        public MapToTableAttribute(string name) => Name = name;
+    }
+}
---
+++ sync/Syncer/Database/MapToTableFieldAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Syncer.Database
+{
+    public class MapToTableFieldAttribute : Attribute
+    {
+        public string Name { get; }
+
+        public MapToTableFieldAttribute(string name) => Name = name;
+    }
+}
