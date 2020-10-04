242a3531bc2105351d0889baf469f2125ec2b3a1
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-04 21:41:39 +0300

    feat(elasticsearch): Add impleemntation for index name on the base document

--- sync/Syncer/Elasticsearch/Documents/BaseDocument.cs
+++ sync/Syncer/Elasticsearch/Documents/BaseDocument.cs
@@ -11,6 +11,8 @@ namespace Syncer.Elasticsearch.Documents
         [Keyword(Name = "_docType")]
         public string DocType { get; set; }
 
+        [Text(Ignore = true)] public string IndexName => nameof(GetType);
+
         protected BaseDocument()
         {
             DocType = $"{GetType().FullName}, {GetType().Assembly.GetName().Name}";
