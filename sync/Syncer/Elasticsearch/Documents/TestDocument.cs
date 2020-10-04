6650a3ba8c4b181388cb670db7c3966adaa303ab
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-04 19:35:03 +0300

    feat(test-document): Implement the test document

---
+++ sync/Syncer/Elasticsearch/Documents/TestDocument.cs
@@ -0,0 +1,14 @@
+﻿using Syncer.Database;
+
+namespace Syncer.Elasticsearch.Documents
+{
+    [MapToTable("test")]
+    public class TestDocument : BaseDocument
+    {
+        [MapToTableField("name")]
+        public string Name { get; set; }
+
+        [MapToTableField("id")]
+        public override string Id { get; set; }
+    }
+}
