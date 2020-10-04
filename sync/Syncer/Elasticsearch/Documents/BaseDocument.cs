ea56d11ac60a84423c0340f89948ad80a5440be8
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-04 19:34:47 +0300

    feat(document): Add base document

---
+++ sync/Syncer/Elasticsearch/Documents/BaseDocument.cs
@@ -0,0 +1,19 @@
+﻿using Nest;
+
+namespace Syncer.Elasticsearch.Documents
+{
+    [ElasticsearchType(IdProperty = "Id", Name = "_doc")]
+    public abstract class BaseDocument
+    {
+        [Keyword]
+        public abstract string Id { get; set; }
+
+        [Keyword(Name = "_docType")]
+        public string DocType { get; set; }
+
+        protected BaseDocument()
+        {
+            DocType = $"{GetType().FullName}, {GetType().Assembly.GetName().Name}";
+        }
+    }
+}
