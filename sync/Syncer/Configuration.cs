71f2a8ce0c022436518238c35b70f1eb260418ce
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 23:06:00 +0300

    feat(di): Register handlers

--- sync/Syncer/Configuration.cs
+++ sync/Syncer/Configuration.cs
@@ -9,8 +9,6 @@ using Syncer.Configuration;
 using Syncer.Contracts;
 using Syncer.Elasticsearch;
 using Syncer.Elasticsearch.Abstractions;
-using Syncer.Elasticsearch.Documents;
-using Syncer.Factory;
 using Syncer.Services;
 using Syncer.Services.Handlers;
 using Syncer.Services.Visitors;
@@ -31,8 +29,8 @@ namespace Syncer
 
         private static void ConfigureServices(IServiceCollection serviceCollection)
         {
-            serviceCollection.AddFactory<IDeleteHandler<TestDocument>, TestDocumentDeleteHandler>();
-            serviceCollection.AddFactory<ICreateHandler<TestDocument>, TestDocumentCreateHandler>();
+            serviceCollection.AddTransient<IDeleteHandler, TestDocumentDeleteHandler>();
+            serviceCollection.AddTransient<ICreateHandler, TestDocumentCreateHandler>();
 
             var applicationConfiguration = ConfigurationFactory.CreateConfiguration();
 
