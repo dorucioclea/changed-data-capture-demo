3d2edc1b5d07d11c42c8088a053c45fcb2e167a6
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 23:04:05 +0300

    feat(handler): Create a handler descriptor interface that will tell you what type it handles

---
+++ sync/Syncer/Contracts/IHandlerDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Syncer.Database;
+using Syncer.Elasticsearch.Documents;
+
+namespace Syncer.Contracts
+{
+    public interface IHandlerDescriptor<T> where T : BaseDocument, new()
+    {
+        string Name() =>
+            typeof(T).GetCustomAttributes(typeof(MapToTableAttribute), true).FirstOrDefault() is MapToTableAttribute mapToTableAttribute
+                ? mapToTableAttribute.Name
+                : null;
+    }
+}
