464375ef0494b88b5ea659d7693c4078c6421d38
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 23:05:49 +0300

    refactor(handler): Actually attempt to map the property by looking up the data stored into its map to field attribute

--- sync/Syncer/Services/Handlers/HandlerBase.cs
+++ sync/Syncer/Services/Handlers/HandlerBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using MySqlCdc.Events;
 using Syncer.Configuration;
+using Syncer.Database;
 using Syncer.Elasticsearch.Documents;
 
 namespace Syncer.Services.Handlers
@@ -16,7 +18,6 @@ namespace Syncer.Services.Handlers
             foreach (var row in rows)
             {
                 var newRow = new T();
-                var type = newRow.GetType();
 
                 for (var i = 0; i < row.Cells.Count; i++)
                 {
@@ -30,8 +31,9 @@ namespace Syncer.Services.Handlers
                         throw new InvalidOperationException($"Could not process create for type {nameof(T)}");
                     }
 
+                    var prop = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .FirstOrDefault(p => p.GetCustomAttributes(typeof(MapToTableFieldAttribute), false).FirstOrDefault() is MapToTableFieldAttribute mapToField && mapToField.Name == rowPropertyName.Name);
 
-                    var prop = type.GetProperty(rowPropertyName.Name);
                     prop?.SetValue(newRow, rowCell, null);
                 }
 
