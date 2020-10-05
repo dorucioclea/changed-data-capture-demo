a59d713ac202f7f70f3fee35351dbfa1f0d0783c
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 22:01:48 +0300

    feat(handler): Implement simplist delete handler

---
+++ sync/Syncer/Services/Handlers/TestDocumentDeleteHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Contracts;
+using Syncer.Elasticsearch.Abstractions;
+using Syncer.Elasticsearch.Documents;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Services.Handlers
+{
+    public class TestDocumentDeleteHandler: HandlerBase<TestDocument>, IDeleteHandler
+    {
+        private readonly IElasticsearchRepository _elasticsearchRepository;
+
+        public TestDocumentDeleteHandler(IElasticsearchRepository elasticsearchRepository)
+        {
+            _elasticsearchRepository = elasticsearchRepository;
+        }
+
+        public async ValueTask HandleDelete(DeleteRowsEvent deleteRows, PreProcessInformation preProcessInformation)
+        {
+            var itemsToDelete = GetItemsFrom(deleteRows.Rows, preProcessInformation.TableConfiguration.Columns);
+            var indexName = itemsToDelete.First().IndexName;
+
+            foreach (var item in itemsToDelete)
+            {
+                await _elasticsearchRepository.DeleteAsync<TestDocument>(item.Id, indexName, true);
+            }
+        }
+    }
+}
