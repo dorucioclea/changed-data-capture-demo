a49ca9c563a72942630d7abc2c47f1bad82833cf
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 22:00:23 +0300

    feat(handlers): Add naive handlers

---
+++ sync/Syncer/Contracts/ICreateHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Contracts
+{
+    public interface ICreateHandler 
+    {
+        ValueTask HandleCreate(WriteRowsEvent writeRows, PreProcessInformation preProcessInformation);
+    }
+}
---
+++ sync/Syncer/Contracts/IDeleteHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Contracts
+{
+    public interface IDeleteHandler
+    {
+        ValueTask HandleDelete(DeleteRowsEvent deleteRows, PreProcessInformation preProcessInformation);
+    }
+}
