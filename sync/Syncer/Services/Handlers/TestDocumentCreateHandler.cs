388e4202ff1e4283077b8519f3479eec5ee09bed
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-05 22:01:35 +0300

    feat(handler): Implement simplist create handler

---
+++ sync/Syncer/Services/Handlers/TestDocumentCreateHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Contracts;
+using Syncer.Elasticsearch.Abstractions;
+using Syncer.Elasticsearch.Documents;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Services.Handlers
+{
+    public class TestDocumentCreateHandler : HandlerBase<TestDocument>, ICreateHandler
+    {
+        private readonly IElasticsearchRepository _elasticsearchRepository;
+
+        public TestDocumentCreateHandler(IElasticsearchRepository elasticsearchRepository)
+        {
+            _elasticsearchRepository = elasticsearchRepository;
+        }
+
+        public async ValueTask HandleCreate(WriteRowsEvent writeRows, PreProcessInformation preProcessInformation)
+        {
+            var newItems = GetItemsFrom(writeRows.Rows, preProcessInformation.TableConfiguration.Columns);
+            var indexName = newItems.First().IndexName;
+
+            await _elasticsearchRepository.BulkAsync(newItems, indexName, true);
+        }
+    }
+}
