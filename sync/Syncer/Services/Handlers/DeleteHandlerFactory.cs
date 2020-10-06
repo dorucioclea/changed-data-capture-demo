62d17aefb11b2977ef69d5b0593c57f6e9418bfc
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-06 10:41:11 +0300

    feat(factory): Implement delete handler factory

---
+++ sync/Syncer/Services/Handlers/DeleteHandlerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Syncer.Contracts;
+
+namespace Syncer.Services.Handlers
+{
+    public class DeleteHandlerFactory : IDeleteHandlerFactory
+    {
+        private readonly List<IDeleteHandler> _deleteHandlers;
+        public DeleteHandlerFactory(List<IDeleteHandler> deleteHandlers)
+        {
+            _deleteHandlers = deleteHandlers;
+        }
+
+
+        public IDeleteHandler GetDeleteHandler(string tableName)
+        {
+            var handler = _deleteHandlers.SingleOrDefault(handlerItem => handlerItem.HandledTableName == tableName);
+
+            if (handler == null)
+            {
+                throw new NullReferenceException($"Could not get a delete handler for {tableName}");
+            }
+
+            return handler;
+        }
+    }
+}
