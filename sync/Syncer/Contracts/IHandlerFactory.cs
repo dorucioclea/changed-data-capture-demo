24b9a75a19d5c4106f4735f4169405a8ffc2f114
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-06 10:36:30 +0300

    feat(factory): Implement handlers factory

---
+++ sync/Syncer/Contracts/IHandlerFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Contracts
+{
+    public interface IHandlerFactory
+    {
+        ICreateHandler GetCreateHandler(string tableName);
+        IDeleteHandler GetDeleteHandler(string tableName);
+    }
+}
---
+++ sync/Syncer/Services/Handlers/HandlerFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Syncer.Contracts;
+
+namespace Syncer.Services.Handlers
+{
+    public class HandlerFactory : IHandlerFactory
+    {
+        private readonly List<ICreateHandler> _createHandlers;
+        private readonly List<IDeleteHandler> _deleteHandlers;
+
+        public HandlerFactory(List<ICreateHandler> createHandlers, List<IDeleteHandler> deleteHandlers)
+        {
+            _createHandlers = createHandlers;
+            _deleteHandlers = deleteHandlers;
+        }
+
+        public ICreateHandler GetCreateHandler(string tableName)
+        {
+            var handler = _createHandlers.SingleOrDefault(handlerItem => handlerItem.HandledTableName == tableName);
+
+            if (handler == null)
+            {
+                throw new NullReferenceException($"Could not get a create handler for {tableName}");
+            }
+
+            return handler;
+        }
+
+        public IDeleteHandler GetDeleteHandler(string tableName)
+        {
+            var handler = _deleteHandlers.SingleOrDefault(handlerItem => handlerItem.HandledTableName == tableName);
+
+            if (handler == null)
+            {
+                throw new NullReferenceException($"Could not get a delete handler for {tableName}");
+            }
+
+            return handler;
+        }
+    }
+}
