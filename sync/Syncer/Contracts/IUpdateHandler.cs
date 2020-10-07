dde79056d190a2de18991c079920a0a75a6d001f
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2020-10-07 18:26:05 +0300

    feat(handler): Add update handler

---
+++ sync/Syncer/Contracts/IUpdateHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using MySqlCdc.Events;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Contracts
+{
+    public interface IUpdateHandler : IHandler
+    {
+        ValueTask HandleUpdate(UpdateRowsEvent updateRows, PreProcessInformation preProcessInformation);
+    }
+}
---
+++ sync/Syncer/Services/Handlers/TestDocumentUpdateHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MySqlCdc.Events;
+using Syncer.Configuration;
+using Syncer.Contracts;
+using Syncer.Elasticsearch.Documents;
+using Syncer.Services.Visitors;
+
+namespace Syncer.Services.Handlers
+{
+    public class TestDocumentUpdateHandler : HandlerBase<TestDocument, int>, IUpdateHandler
+    {
+        public TestDocumentUpdateHandler(IOptions<ElasticSearchConfiguration> elasticSearchConfiguration) : base(
+            elasticSearchConfiguration)
+        {
+
+        }
+
+        public async ValueTask HandleUpdate(UpdateRowsEvent updateRows, PreProcessInformation preProcessInformation)
+        {
+            var beforeUpdateColumnData = updateRows.Rows.Select(row => row.BeforeUpdate).ToList();
+            var afterUpdateColumnData = updateRows.Rows.Select(row => row.AfterUpdate).ToList();
+
+            var itemsBeforeUpdate = GetItemsFrom(beforeUpdateColumnData, 
+                preProcessInformation.TableConfiguration.Columns);
+            
+            var itemsAfterUpdate =
+                GetItemsFrom(afterUpdateColumnData, preProcessInformation.TableConfiguration.Columns);
+
+            var indexName = GetIndexName();
+
+            var repository = GetElasticRepository();
+
+            // update items
+
+        }
+    }
+}
